a7f4a641760b8b67a1f4b3975c68ca93b1c03e3e
Christian Franke <christian.franke@21re.de>
2019-01-23 13:48:42 +0100

    [feat] bool query

--- src/elasticsearch_test.rs
+++ src/elasticsearch_test.rs
@@ -15,6 +15,25 @@ fn test_term_query() {
 }
 
 #[test]
+fn test_bool_query() {
+  let query = QueryRequest::new().with_query(Query::bool_query(
+    vec![Query::term("fieldname", "fieldvalue")],
+    vec![],
+    vec![],
+    vec![],
+    None,
+  ));
+
+  let json = serde_json::to_string(&query).unwrap();
+
+  assert_that(&json.as_str()).is_equal_to(r#"{"query":{"bool":{"must":[{"term":{"fieldname":"fieldvalue"}}],"filter":[],"must_not":[],"should":[],"minimum_should_match":null}}}"#);
+
+  let actual = serde_json::from_str::<QueryRequest>(&json).unwrap();
+
+  assert_that(&actual).is_equal_to(query);
+}
+
+#[test]
 fn test_prefix_query() {
   let query = QueryRequest::new().with_query(Query::prefix("bla", "blub"));
   let json = serde_json::to_string(&query).unwrap();
